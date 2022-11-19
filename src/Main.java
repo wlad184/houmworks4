6e8bf350214dcf35277105c1b9a587e208c835d4
wlad184 <evel184@yandex.ru>
2022-11-19 10:55:29 +0700

    task 2

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,26 @@ public class Main {
     public static void main(String[] args) {
         //task #1
         int clientOS = 1;
-        if (clientOS == 0){
+        if (clientOS == 0) {
             System.out.println("Установите версию приложения для iOS по ссылке");
-        }else{
+        } else {
             System.out.println("Установите версию приложения для Android по ссылке");
         }
+        System.out.println();
+
+        //task #2
+        int clientDeviceYear = 2014;
+        if (clientOS == 0 && clientDeviceYear < 2015) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+           }else if (clientOS == 0 && clientDeviceYear >= 2015) {
+            System.out.println("Установите стандартную версию приложения для iOS по ссылке");
+        }
+        if (clientOS ==1 && clientDeviceYear < 2015) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+           }else if (clientOS == 1 && clientDeviceYear >= 2015) {
+            System.out.println("Установите стандартную версию приложения для Android по ссылке");
+        }
+
+
     }
 }
