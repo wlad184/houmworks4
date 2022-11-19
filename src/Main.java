b04fb4889d30cb6af0ffcf7dec725a0e4366fb23
wlad184 <evel184@yandex.ru>
2022-11-19 10:06:29 +0700

    hw4

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        //task #1
+        int clientOS = 1;
+        if (clientOS == 0){
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        }else{
+            System.out.println("Установите версию приложения для Android по ссылке");
+        }
+    }
+}
